1f3720e8663424ed66c4d01ce3737d73ab63d3a7
Cathleen Runde <cathleenrunde@Cathleens-MBP.home>
2021-09-13 07:05:01 -0700

    home screen, nav, app.jsx

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,14 +1,54 @@
+import { Layout } from '../../components'
 import './Home.css'
 
+const Home = (props) => {
 
 
   return (
-    
+
+    <Layout user={props.user}>
       <div className='home'>
+        <body>
+          <img src=''
+            alt='home-img'
+            id='home-img' />
+          <div className='home-pg-content'>
+            <div className='home-title-logos'>
+              <h1>Tech Care</h1>
+              <div className='home-logos-desc'>
+                <div className='home-vs'>
+                  <img src=''
+                    id='vs-code'
+                    alt='vs-code' />
+                  <p></p>
+                </div>
+                <div className='home-cpu'>
+                  <img src=""
+                    id='cpu'
+                    src='cpu' />
+                </div>
+                <div className='home-ram'>
+                  <img src=''
+                    id='ram'
+                    alt='ram' />
+                </div>
+              </div>
+            </div>
+            <div className='home-mission-statment-link'>
+              <div className='home-mission-statement'>
+                <p></p>
+              </div>
+              <div className='home-browse-selection-link'>
+
+              </div>
+            </div>
+          </div>
+        </body>
         <h1>This is our home page</h1>
       </div>
-    
+    </Layout>
   )
+}
 
 
 export default Home
