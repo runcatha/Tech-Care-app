046e7ad6e7edc8025a7e35b926aef05a6c638c97
estherryu77 <estherryu77@gmail.com>
2021-09-13 07:19:22 -0600

    added components

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -3,10 +3,9 @@ import { NavLink } from 'react-router-dom'
 
 const authenticatedOptions = (
     <>
-    <NavLink className="link" to="/add-laptop">Add Laptpop</NavLink>
-    <NavLink className="link" to="/edit-laptop">Edit Laptop</NavLink>
-    <NavLink className="link" to="/delete-laptop">Delete Laptop</NavLink>
-        <NavLink className="link" to="/sign-out">Sign Out</NavLink>
+    <Link className="link" to="/add-laptop">Add Laptpop</NavLink>
+    <Link className="link" to="/laptops/:id/edit">Edit Laptop</NavLink>
+    <NavLink className="link" to="/sign-out">Sign Out</NavLink>
     </>
 )
 const unauthenticatedOptions = (
@@ -19,7 +18,7 @@ const alwaysOptions = (
     <>
     <NavLink className="link" to="/">Home</NavLink>
     <NavLink className="link" to="/laptops">Listing</NavLink>
-    <NavLink className="link" to="/details">Details Page</NavLink>
+    <Link className="link" to="/laptops/:id">Details Page</NavLink>
     </>
 )
 const Nav = ({ user }) => {
