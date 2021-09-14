b806f2f6d9608c47b413a3a4be08831718c85fcf
Cathleen Runde <cathleenrunde@Cathleens-MBP.home>
2021-09-14 09:37:46 -0700

    edit pg

--- client/src/screens/LaptopEdit/LaptopEdit.jsx
+++ client/src/screens/LaptopEdit/LaptopEdit.jsx
@@ -47,14 +47,15 @@ const LaptopEdit = (props) => {
 
   return (
     <Layout user={props.user}>
-      <div className='laptop-edit'>
-        <div className='image-container'>
-          <img
-            className='edit-laptop-image'
-            src={laptop.image_url}
-            alt={laptop.name}
-          />
-          <form onSubmit={handleSubmit}>
+      <div className='edit-form-content-container'>
+        <div className='doggo-container'>
+          <img src='https://i.imgur.com/s43Gtl9.jpg'
+            id='doggo-programmer'
+            alt='doggo-programmer' />
+          <div className='doggo-padding'></div>
+        </div>
+        <form className='edit-form' onSubmit={handleSubmit}>
+          <div className='edit-input-container'>
             <input
               className='edit-input-image-link'
               placeholder='Image Link'
@@ -63,76 +64,73 @@ const LaptopEdit = (props) => {
               required
               onChange={handleChange}
             />
-          </form>
-        </div>
-        <form className='edit-form' onSubmit={handleSubmit}>
-          <input
-            className='input-name'
-            placeholder='Name'
-            value={laptop.name}
-            name='name'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
-          <input
-            className='input-price'
-            placeholder='Price'
-            value={laptop.price}
-            name='price'
-            required
-            onChange={handleChange}
-          />
-          <textarea
-            className='textarea-description'
-            rows={10}
-            cols={78}
-            placeholder='Description'
-            value={laptop.description}
-            name='description'
-            required
-            onChange={handleChange}
-          />
-          
-          <input
-            className='input-processor'
-            placeholder='Processor'
-            value={laptop.processor}
-            name='processor'
-            required
-            onChange={handleChange}
-          />
-          <input
-            className='input-memory'
-            placeholder='Memory'
-            value={laptop.memory}
-            name='memory'
-            required
-            onChange={handleChange}
-          />
-          <input
-            className='input-hardrive'
-            placeholder='Hardrive'
-            value={laptop.hardrive}
-            name='hardrive'
-            required
-            onChange={handleChange}
-          />
-          <input
-            className='input-buy-link'
-            placeholder='Buy-Link'
-            value={laptop.buy_link}
-            name='buy-link'
-            required
-            onChange={handleChange}
-          />
+            <input
+              className='input-name'
+              placeholder='Name'
+              value={laptop.name}
+              name='name'
+              required
+              autoFocus
+              onChange={handleChange}
+            />
+            <input
+              className='input-price'
+              placeholder='Price'
+              value={laptop.price}
+              name='price'
+              required
+              onChange={handleChange}
+            />
+            <textarea
+              className='input-description'
+              placeholder='Description'
+              value={laptop.description}
+              name='description'
+              required
+              onChange={handleChange}
+            />
 
-          <button type='submit' className='save-button'>
-            Save
-          </button>
+            <input
+              className='input-processor'
+              placeholder='Processor'
+              value={laptop.processor}
+              name='processor'
+              required
+              onChange={handleChange}
+            />
+            <input
+              className='input-memory'
+              placeholder='Memory'
+              value={laptop.memory}
+              name='memory'
+              required
+              onChange={handleChange}
+            />
+            <input
+              className='input-hardrive'
+              placeholder='Hardrive'
+              value={laptop.hardrive}
+              name='hardrive'
+              required
+              onChange={handleChange}
+            />
+            <input
+              className='input-buy-link'
+              placeholder='Buy-Link'
+              value={laptop.buy_link}
+              name='buy-link'
+              required
+              onChange={handleChange}
+            />
+          </div>
+          <div className='edit-form-button-div'>
+            <button type='submit' className='save-button'>
+              Submit Edit
+            </button>
+          </div>
         </form>
-      </div>
-    </Layout>
+      </div >
+    </Layout >
   )
 }
 
