5c1d22f0f412a1f998df89a626d5418a98ffbabe
estherryu77 <estherryu77@gmail.com>
2021-09-14 09:23:33 -0600

    added search and did some styling

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,7 @@
 import Layout from './Layout/Layout'
 import Laptop from './Laptop/Laptop'
-// import Search from './Search/Search'
+import Search from './Search/Search'
 // import Sort from './Sort/Sort'
 import LaptopCards from './LaptopCards/LaptopCards'
 // add Search and Sort later in line 7
-export { Layout, Laptop, LaptopCards }
+export { Layout, Laptop, Search, LaptopCards }
--- client/src/screens/Laptops/Laptops.jsx
+++ client/src/screens/Laptops/Laptops.jsx
@@ -8,26 +8,40 @@ import {Link} from 'react-router-dom'
 
 import { Layout, Laptop } from '../../components'
 import { getLaptops } from '../../services/laptops'
+import Search from '../../components/Search/Search'
 
 
 const Laptops = (props) => {
   const [laptops, setLaptops] = useState([])
+  const [searchResult, setSearchResult] = useState([])
 
   useEffect(() => {
     const fetchLaptops = async () => {
       const allLaptops = await getLaptops()
       setLaptops(allLaptops)
+      setSearchResult(allLaptops)
 
     }
     fetchLaptops()
   }, [])
 
+  const handleSearch = (event) => {
+    const results = laptops.filter((laptop) =>
+      laptop.name.toLowerCase().includes(event.target.value.toLowerCase())
+    )
+    setSearchResult(results)
+    
+  }
+
+  const handleSubmit = (event) => event.preventDefault()
+
 
   return (
     <Layout user={props.user}>
+    <Search onSubmit={handleSubmit} handleSearch={handleSearch} />
       
       <div className='laptops'>
-        {laptops.map((laptop, index) =>
+        {searchResult.map((laptop, index) =>
           // <Link className="link" to="/laptops/:id">
           <Link className="link" to={`/laptops/${laptop._id}`}>
           <Laptop
