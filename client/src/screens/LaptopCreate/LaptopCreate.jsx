132bb47968c2699a02a3b803b440a1076a0bddb4
Cathleen Runde <cathleenrunde@Cathleens-MBP.home>
2021-09-14 10:25:33 -0700

    create form

--- client/src/screens/LaptopCreate/LaptopCreate.jsx
+++ client/src/screens/LaptopCreate/LaptopCreate.jsx
@@ -38,86 +38,90 @@ const LaptopCreate = (props) => {
   return (
     <Layout user={props.user}>
       <div className='create-form-content-container'>
-        <img src='https://i.imgur.com/PenAzS5.jpg'
-          id='doggo-programmer'
-          alt='doggo-programmer' />
+        <div className='create-laptop-image-container'>
+          <img src='https://i.imgur.com/PenAzS5.jpg'
+            id='create-laptop'
+            alt='create-laptop' />
+          <div className='create-laptop-padding'></div>
+        </div>
         <form className='create-form' onSubmit={handleSubmit}>
-          <input
-            className='input-image-link'
-            placeholder='Image'
-            value={laptop.image_url}
-            name='image_url'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
-          <input
-            className='input-name'
-            placeholder='Name'
-            value={laptop.name}
-            name='name'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
-          <input
-            className='input-price'
-            placeholder='Price'
-            value={laptop.price}
-            name='price'
-            required
-            onChange={handleChange}
-          />
-          <textarea
-            className='textarea-description'
-            rows={10}
-            placeholder='Description'
-            value={laptop.description}
-            name='description'
-            required
-            onChange={handleChange}
-          />
-          <input
-            className='input-processor'
-            placeholder='Processor'
-            value={laptop.processor}
-            name='processor'
-            required
-            onChange={handleChange}
-          />
-          <input
-            className='input-memory'
-            placeholder='Memory'
-            value={laptop.memory}
-            name='memory'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
-          <input
-            className='input-hardrive'
-            placeholder='Hardrive'
-            value={laptop.hardrive}
-            name='hardrive'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
-          <input
-            className='input-buy-link'
-            placeholder='Buylink'
-            value={laptop.buy_link}
-            name='buylink'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
+          <div className='create-input-container'>
+            <input
+              className='input-image-link'
+              placeholder='Image'
+              value={laptop.image_url}
+              name='image_url'
+              required
+              autoFocus
+              onChange={handleChange}
+            />
+            <input
+              className='input-name'
+              placeholder='Name'
+              value={laptop.name}
+              name='name'
+              required
+              autoFocus
+              onChange={handleChange}
+            />
+            <input
+              className='input-price'
+              placeholder='Price'
+              value={laptop.price}
+              name='price'
+              required
+              onChange={handleChange}
+            />
+            <textarea
+              className='input-description'
+              placeholder='Description'
+              value={laptop.description}
+              name='description'
+              required
+              onChange={handleChange}
+            />
+            <input
+              className='input-processor'
+              placeholder='Processor'
+              value={laptop.processor}
+              name='processor'
+              required
+              onChange={handleChange}
+            />
+            <input
+              className='input-memory'
+              placeholder='Memory'
+              value={laptop.memory}
+              name='memory'
+              required
+              autoFocus
+              onChange={handleChange}
+            />
+            <input
+              className='input-hardrive'
+              placeholder='Hardrive'
+              value={laptop.hardrive}
+              name='hardrive'
+              required
+              autoFocus
+              onChange={handleChange}
+            />
+            <input
+              className='input-buy-link'
+              placeholder='Buylink'
+              value={laptop.buy_link}
+              name='buylink'
+              required
+              autoFocus
+              onChange={handleChange}
+            />
+          </div>
+          <div className='create-form-button-div'>
+            <button type='submit' className='create-form-submit-button'>
+              Submit
+            </button>
+          </div>
         </form>
-      </div>
-      <div className='add-form-button-div'>
-        <button type='submit' className='add-form-submit-button'>
-          Submit
-        </button>
       </div>
     </Layout>
   )
