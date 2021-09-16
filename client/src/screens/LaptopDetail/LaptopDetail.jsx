2a86a9b129dd69783e9e79fa3a4f72b5df73d937
SlayerT26 <456theochang@gmail.com>
2021-09-16 16:25:22 -0700

    detail page finished

--- client/src/screens/LaptopDetail/LaptopDetail.jsx
+++ client/src/screens/LaptopDetail/LaptopDetail.jsx
@@ -91,12 +91,14 @@ const LaptopDetail = (props) => {
               <Link className="edit-button" to={`/laptops/${laptop._id}/edit`}>
                 <button className="editbutton">Edit</button>
               </Link>
-              <button
-                className="delete-button"
-                onClick={() => deleteLaptop(laptop._id)}
-              >
-                Delete
-              </button>
+              <div>
+                <button
+                  className="delete-button"
+                  onClick={() => deleteLaptop(laptop._id)}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
           </div>
         </div>
