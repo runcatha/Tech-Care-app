e9e1675a2fa66747fc3e5dad5fece29fc7f44a50
Cathleen Runde <cathleenrunde@Cathleens-MBP.home>
2021-09-16 19:21:53 -0700

    review form

--- client/src/screens/LaptopDetail/LaptopDetail.jsx
+++ client/src/screens/LaptopDetail/LaptopDetail.jsx
@@ -28,6 +28,9 @@ const LaptopDetail = (props) => {
     description: '',
   })
 
+  const { handleRedirect, user } = props
+
+
   useEffect(() => {
     const fetchLaptop = async () => {
       const laptop = await getLaptop(id);
@@ -47,12 +50,22 @@ const LaptopDetail = (props) => {
   }
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
-    laptop.reviews.push(review)
-    setLaptop(laptop)
-    await updateLaptop(id, laptop)
-    setToggleFetch((toggleFetch) => !toggleFetch)
-    history.push('/laptops/:id')
+    if (user !== null) {
+      event.preventDefault()
+      laptop.reviews.push(review)
+      setLaptop(laptop)
+      await updateLaptop(id, laptop)
+      setToggleFetch((toggleFetch) => !toggleFetch)
+      setReview({
+        author: '',
+        rating: '',
+        description: '',
+      })
+      history.push('/laptops/:id')
+    } else {
+      alert('Please sign in first')
+      history.push('/sign-in')
+    }
   }
 
   const handleDelete = async (event) => {
@@ -68,7 +81,7 @@ const LaptopDetail = (props) => {
   }
 
   return (
-    <Layout user={props.user}>
+    <Layout user={user}>
       <div className="laptop-detail">
         <div className="detail">
           <h1>Description</h1>
